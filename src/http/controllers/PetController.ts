ab47a3dc2613761606f75f5ff306824a94df0880
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-06 19:25:54 -0300

    feat: create pet on controller post

--- src/http/controllers/PetController.ts
+++ src/http/controllers/PetController.ts
@@ -6,6 +6,26 @@ export class PetControllers {
   async get(request: Request, response: Response) {}
   async getbyId(request: Request, response: Response) {}
   async put(request: Request, response: Response) {}
-  async post(request: Request, response: Response) {}
+  async post(request: Request, response: Response) {
+    const { nome, idade, especie, raca, adocao } = request.body;
+
+    const isAdotted = adocao === "true" ? true : false;
+
+    try {
+      const pets = await prisma.pet.create({
+        data: {
+          nome,
+          idade,
+          especie,
+          raca,
+          adocao: isAdotted,
+        },
+      });
+
+      return response.json(pets);
+    } catch (error) {
+      return response.json({ message: error });
+    }
+  }
   async delete(request: Request, response: Response) {}
 }
