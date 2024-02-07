a5e9b3f46a2a65906853ecd803fdd08dfced52f4
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-06 22:36:39 -0300

    chore: install cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 
@@ -7,5 +8,6 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(router);
+app.use(cors);
 
-app.listen(PORT, () => console.log("Server is running on port", PORT));
+app.listen(PORT, () => console.log("http://localhost:3001/pets"));
