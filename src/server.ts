2d8c7a9324bd756027d43f3da1117bd42acd4973
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-06 22:59:32 -0300

    chore: change port

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import cors from "cors";
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(router);
 app.use(cors);
 
-app.listen(PORT, () => console.log("http://localhost:3001/pets"));
+app.listen(PORT, () => console.log("Running"));
