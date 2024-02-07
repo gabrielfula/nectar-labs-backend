45c039cad485c38994f8708079e626e7df5d47e9
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-06 23:42:57 -0300

    chore: add cors to connect

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
-app.use(cors);
 
 app.listen(PORT, () => console.log("Running"));
